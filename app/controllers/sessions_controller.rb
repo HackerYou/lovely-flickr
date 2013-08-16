0296b0671142896825a0f9e679c9f3e5626b2870
Edward Ocampo-Gooding <edward@edwardog.net>
2013-08-15 20:57:53 -0400

    Be nice about password fails

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
       session[:user_id] = @user.id
       redirect_to photos_path
     else
-      flash[:error] = "You stupid fuck you can't even type in a password."
+      flash[:error] = "You silly person; that email/password combination doesn’t seem right."
       redirect_to new_session_path
     end
   end
